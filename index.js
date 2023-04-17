6efbd1244cbc75496c8ed3f1060daa1fd33e62f2
Katherine Vilinski-Mazur <Katherine.Vilinski-Mazur@skoltech.ru>
2023-04-17 22:54:16 +0300

    wtf tests

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 //Напиши метод isAnagrams(), который принимает два строчных аргумента
 // и выводит true, если строки являются анаграммами, и false, если строки ими не являются.
 
-function isAnagrams(str1, str2){
-
-}
+const isAnagrams = (str1, str2) => {
+  return str1.split("").sort().join() == str2.split("").sort().join()
+    ? true
+    : false;
+};
+console.log(isAnagrams("lem2on", "mel2on"));
---
+++ spec/index.spec.js
@@ -0,0 +1,14 @@
+const isAnagrams = require("../index");
+
+test("Функция должна быть определена", () => {
+  expect(isAnagrams("lemon", "melon")).toBeDefined();
+});
+
+test("Функция должна находить анаграммы", () => {
+  expect(isAnagrams("melon", "lemon")).toBe(true);
+  expect(isAnagrams("melo2n", "lemon2")).toBe(true);
+  expect(isAnagrams("lemon", "melon")).toBeTruthy();
+  expect(isAnagrams("le2mon", "melon")).toBeFalsy();
+  expect(isAnagrams("melon", "melon")).toBe(true);
+  expect(isAnagrams("kilso", "osilk")).toBeTruthy();
+});
